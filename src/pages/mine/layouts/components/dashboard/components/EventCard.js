9bc41084e539cf51447f148987f9b90b32b4a7be
Rstar-1 <rajshetye.5855@gmail.com>
2024-07-27 21:07:02 +0530

    g

--- src/pages/mine/layouts/components/dashboard/components/EventCard.js
+++ src/pages/mine/layouts/components/dashboard/components/EventCard.js
@@ -1,186 +1,136 @@
 import React from "react";
-import {
-  CircularProgressbar
-} from "react-circular-progressbar";
+import { CircularProgressbar } from "react-circular-progressbar";
 import "react-circular-progressbar/dist/styles.css";
 import FeatherIcon from "feather-icons-react";
 
 const EventCard = () => {
   const percentage = 60;
   return (
-    <div className="grid-cols-3 event gap-12 w-full mtpx30 items-start">
-      <div className="bgforth px20 py26 rounded-5 event-h">
-        <div className="grid-cols-2">
-          <div className="bordr">
-            <div className="bordb py18">
-              <div className="event-progress mx-auto relative">
-                <CircularProgressbar
-                  value={percentage}
-                  strokeWidth={6}
-                ></CircularProgressbar>
-                <div className="flex items-center justify-center absolute top-0 left-0 event-progress">
-                  <p className="textwhite fsize14">{`${percentage}`}%</p>
-                </div>
-              </div>
-              <p className="mtpx10 textwhite fsize14 text-center">
-                Lorem Ipsum
-              </p>
-            </div>
-            <div className="py18">
-              <div className="event-progress mx-auto relative">
-                <CircularProgressbar
-                  value={percentage}
-                  strokeWidth={6}
-                ></CircularProgressbar>
-                <div className="flex items-center justify-center absolute top-0 left-0 event-progress">
-                  <p className="textwhite fsize14">{`${percentage}`}%</p>
-                </div>
-              </div>
-              <p className="mtpx10 textwhite fsize14 text-center">
-                Lorem Ipsum
-              </p>
-            </div>
-          </div>
-          <div className="">
-            <div className="bordb py18">
-              <div className="event-progress mx-auto relative">
-                <CircularProgressbar
-                  value={percentage}
-                  strokeWidth={6}
-                ></CircularProgressbar>
-                <div className="flex items-center justify-center absolute top-0 left-0 event-progress">
-                  <p className="textwhite fsize14">{`${percentage}`}%</p>
-                </div>
-              </div>
-              <p className="mtpx10 textwhite fsize14 text-center">
-                Lorem Ipsum
-              </p>
-            </div>
-            <div className="py18">
-              <div className="event-progress mx-auto relative">
-                <CircularProgressbar
-                  value={percentage}
-                  strokeWidth={6}
-                ></CircularProgressbar>
-                <div className="flex items-center justify-center absolute top-0 left-0 event-progress">
-                  <p className="textwhite fsize14">{`${percentage}`}%</p>
-                </div>
-              </div>
-              <p className="mtpx10 textwhite fsize14 text-center">
-                Lorem Ipsum
-              </p>
-            </div>
-          </div>
+    <div className="grid-cols-2 event gap-12 w-full mtpx30 items-start">
+      <div className="rounded-5 sm-rounded-5">
+        <div className="table-w">
+          <table className="w-full">
+            <thead>
+              <tr>
+                <th className="fsize13 textwhite font-300 table-colsm">
+                  <p>SectionId</p>
+                </th>
+                <th className="fsize13 textwhite font-300 table-collg">
+                  <p>Image</p>
+                </th>
+                <th className="fsize13 textwhite font-300 table-collg">
+                  <p>Title</p>
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td className="fsize13 textgray  font-300 table-colsm">
+                  <p>e.sectionid</p>
+                </td>
+                <td className="fsize13 textgray  font-300 table-collg">
+                  <img
+                    src="https://api-dev-minimal-v6.vercel.app/assets/images/avatar/avatar-2.webp"
+                    className="table-img"
+                    alt="table-img"
+                  />
+                </td>
+                <td className="fsize13 textgray font-300 table-collg">
+                  <p>e.title</p>
+                </td>
+              </tr>
+            </tbody>
+          </table>
         </div>
       </div>
-      <div className="bgforth px20 py26 rounded-5 event-h">
-        <h6 className="fsize20 my1 textwhite leading">Sticky Notes</h6>
-        <div className="notes-h mtpx16 pbpx10">
-          <div className=" grid-cols-1 gap-12">
-            <div className="flex items-center justify-between bg-glass2 rounded-5 p8">
-              <div className="w-90">
-                <p className="fsize14 line-clamp1 font-300 textwhite">
-                  Top Courses Top Courses Top Courses Top Courses Top Courses
-                  Top Courses Top Courses
-                </p>
+      <div>
+        <div className="grid-cols-2 gap-12">
+          <div className="bgforth px20 py26 rounded-5">
+            <div className="flex items-center justify-between">
+              <div className="flex items-start">
+                <div className="home-icon flex items-center justify-center rounded-5">
+                  <CircularProgressbar
+                    value={percentage}
+                    strokeWidth={10}
+                  ></CircularProgressbar>
+                </div>
+                <div className="mlpx12">
+                  <h6 className="fsize16 font-300 textwhite my1 leading">
+                    Top Courses
+                  </h6>
+                  <p className="fsize12 font-300 textwhite">Top Courses</p>
+                </div>
               </div>
-              <FeatherIcon className="textdanger" size={18} icon="trash-2" />
+              <p className="textsuccess fsize14">70%</p>
             </div>
-            <div className="flex items-center justify-between bg-glass2 rounded-5 p8">
-              <div className="w-90">
-                <p className="fsize14 line-clamp1 font-300 textwhite">
-                  Top Courses Top Courses Top Courses Top Courses Top Courses
-                  Top Courses Top Courses
-                </p>
+            <div className="flex items-center justify-between mtpx28">
+              <div className="flex items-start">
+                <div className="home-icon flex items-center justify-center rounded-5">
+                  <CircularProgressbar
+                    value={percentage}
+                    strokeWidth={10}
+                  ></CircularProgressbar>
+                </div>
+                <div className="mlpx12">
+                  <h6 className="fsize16 font-300 textwhite my1 leading">
+                    Top Courses
+                  </h6>
+                  <p className="fsize12 font-300 textwhite">Top Courses</p>
+                </div>
               </div>
-              <FeatherIcon className="textdanger" size={18} icon="trash-2" />
+              <p className="textsuccess fsize14">70%</p>
             </div>
-            <div className="flex items-center justify-between bg-glass2 rounded-5 p8">
-              <div className="w-90">
-                <p className="fsize14 line-clamp1 font-300 textwhite">
-                  Top Courses Top Courses Top Courses Top Courses Top Courses
-                  Top Courses Top Courses
-                </p>
+          </div>
+          <div className="bgforth px20 py26 rounded-5">
+            <div className="flex items-center justify-between">
+              <div className="flex items-start">
+                <div className="home-icon flex items-center justify-center rounded-5">
+                  <CircularProgressbar
+                    value={percentage}
+                    strokeWidth={10}
+                  ></CircularProgressbar>
+                </div>
+                <div className="mlpx12">
+                  <h6 className="fsize16 font-300 textwhite my1 leading">
+                    Top Courses
+                  </h6>
+                  <p className="fsize12 font-300 textwhite">Top Courses</p>
+                </div>
               </div>
-              <FeatherIcon className="textdanger" size={18} icon="trash-2" />
+              <p className="textsuccess fsize14">70%</p>
             </div>
-            <div className="flex items-center justify-between bg-glass2 rounded-5 p8">
-              <div className="w-90">
-                <p className="fsize14 line-clamp1 font-300 textwhite">
-                  Top Courses Top Courses Top Courses Top Courses Top Courses
-                  Top Courses Top Courses
-                </p>
+            <div className="flex items-center justify-between mtpx28">
+              <div className="flex items-start">
+                <div className="home-icon flex items-center justify-center rounded-5">
+                  <CircularProgressbar
+                    value={percentage}
+                    strokeWidth={10}
+                  ></CircularProgressbar>
+                </div>
+                <div className="mlpx12">
+                  <h6 className="fsize16 font-300 textwhite my1 leading">
+                    Top Courses
+                  </h6>
+                  <p className="fsize12 font-300 textwhite">Top Courses</p>
+                </div>
               </div>
-              <FeatherIcon className="textdanger" size={18} icon="trash-2" />
-            </div>
-            <div className="flex items-center justify-between bg-glass2 rounded-5 p8">
-              <div className="w-90">
-                <p className="fsize14 line-clamp1 font-300 textwhite">
-                  Top Courses Top Courses Top Courses Top Courses Top Courses
-                  Top Courses Top Courses
-                </p>
-              </div>
-              <FeatherIcon className="textdanger" size={18} icon="trash-2" />
+              <p className="textsuccess fsize14">70%</p>
             </div>
           </div>
         </div>
-      </div>
-      <div className="bgforth px20 py26 rounded-5 event-h">
-        <div className="flex items-center justify-between">
-          <div className="flex items-start">
-            <div className="home-icon bg-glass2 flex items-center justify-center rounded-5">
-              <FeatherIcon icon="grid" className="textprimary flex" size={16} />
-            </div>
-            <div className="mlpx12">
-              <h6 className="fsize16 font-300 textwhite my1 leading">
-                Top Courses
-              </h6>
-              <p className="fsize12 font-300 textwhite">Top Courses</p>
-            </div>
+        <div className="bgforth px20 py10 flex justify-between items-center rounded-5 mtpx10">
+          <h4 className="fsize16 font-400 textwhite my1">Know More</h4>
+          <div className="flex items-center">
+            <p className="fsize14 textwhite font-400">2000</p>
+            <FeatherIcon icon="eye" size="16" className="textsuccess mlpx8" />
           </div>
-          <p className="textsuccess fsize14">70%</p>
         </div>
-        <div className="flex items-center justify-between mtpx24">
-          <div className="flex items-start">
-            <div className="home-icon bg-glass2 flex items-center justify-center rounded-5">
-              <FeatherIcon icon="grid" className="textprimary flex" size={16} />
-            </div>
-            <div className="mlpx12">
-              <h6 className="fsize16 font-300 textwhite my1 leading">
-                Top Courses
-              </h6>
-              <p className="fsize12 font-300 textwhite">Top Courses</p>
-            </div>
+        <div className="bgforth px20 py10 flex justify-between items-center rounded-5 mtpx10">
+          <h4 className="fsize16 font-400 textwhite my1">Know More</h4>
+          <div className="flex items-center">
+            <p className="fsize14 textwhite font-400">2000</p>
+            <FeatherIcon icon="eye" size="16" className="textsuccess mlpx8" />
           </div>
-          <p className="textsuccess fsize14">70%</p>
-        </div>
-        <div className="flex items-center justify-between mtpx24">
-          <div className="flex items-start">
-            <div className="home-icon bg-glass2 flex items-center justify-center rounded-5">
-              <FeatherIcon icon="grid" className="textprimary flex" size={16} />
-            </div>
-            <div className="mlpx12">
-              <h6 className="fsize16 font-300 textwhite my1 leading">
-                Top Courses
-              </h6>
-              <p className="fsize12 font-300 textwhite">Top Courses</p>
-            </div>
-          </div>
-          <p className="textsuccess fsize14">70%</p>
-        </div>
-        <div className="flex items-center justify-between mtpx24">
-          <div className="flex items-start">
-            <div className="home-icon bg-glass2 flex items-center justify-center rounded-5">
-              <FeatherIcon icon="grid" className="textprimary flex" size={16} />
-            </div>
-            <div className="mlpx12">
-              <h6 className="fsize16 font-300 textwhite my1 leading">
-                Top Courses
-              </h6>
-              <p className="fsize12 font-300 textwhite">Top Courses</p>
-            </div>
-          </div>
-          <p className="textsuccess fsize14">70%</p>
         </div>
       </div>
     </div>
